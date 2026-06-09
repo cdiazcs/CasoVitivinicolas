d7c18d07df7c15a9529848bb256a4ec5a1cfbc07
MrMauro <maurizio3324@gmail.com>
2026-06-09 09:48:36 -0500

    configuramos SecurityConfig.java para que permita POST PUT DELETE

--- backend/src/main/java/com/vitivinicolas/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/vitivinicolas/backend/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.Customizer;
 
 @Configuration
 public class SecurityConfig {
@@ -13,7 +14,7 @@ public class SecurityConfig {
 
         http
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> {})
+                .cors(Customizer.withDefaults())
                 .headers(headers -> headers.frameOptions(frame -> frame.disable()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/login").permitAll()
